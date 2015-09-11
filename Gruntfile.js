90e6a7fc8509bd656fe0119ea1841a4c896b22fd
Lars Woxberg <lars.woxberg@gmail.com>
2015-09-11 12:29:29 +0200

    Added grunt task runner.
    For linting, minification and running tests. The automatic tests are not
    working yet.

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+    var config = {
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            all: ['js/ecs.js']
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'js/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        },
+        mocha: {
+            test: {
+                src: ['test/index.html'],
+                options: {
+                    run: true
+                }
+            }
+        }
+    };
+    
+    grunt.initConfig(config);
+    
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-mocha');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    
+    grunt.registerTask('default', ['jshint', 'mocha', 'uglify']);
+
+};
