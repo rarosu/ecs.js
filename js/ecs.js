c4e702eea785588f18800d013128c45acf602b06
Lars Woxberg | Rarosu <lars.woxberg@gmail.com>
2015-12-24 13:07:23 +0100

    Updated clone to take prototypes into account.

--- js/ecs.js
+++ js/ecs.js
@@ -44,46 +44,47 @@ var ECS = (function()
         @return {object} - A clone of obj.
     */
     function clone(obj)
-    {
-        var copy;
+	{
+		var copy;
 
-        // Handle the 3 simple types, and null or undefined
-        if (null === obj || "object" != typeof obj)
-            return obj;
+		// Handle the 3 simple types, and null or undefined
+		if (null === obj || "object" != typeof obj)
+			return obj;
 
-        // Handle Date
-        if (obj instanceof Date)
-        {
-            copy = new Date();
-            copy.setTime(obj.getTime());
-            return copy;
-        }
+		// Handle Date
+		if (obj instanceof Date)
+		{
+			copy = new Date();
+			copy.setTime(obj.getTime());
+			return copy;
+		}
 
-        // Handle Array
-        if (obj instanceof Array)
-        {
-            copy = [];
-            for (var i = 0, len = obj.length; i < len; i++)
-            {
-                copy[i] = clone(obj[i]);
-            }
-            return copy;
-        }
+		// Handle Array
+		if (obj instanceof Array)
+		{
+			copy = [];
+			for (var i = 0, len = obj.length; i < len; i++)
+			{
+				copy[i] = clone(obj[i]);
+			}
+			return copy;
+		}
 
-        // Handle Object
-        if (obj instanceof Object)
-        {
-            copy = {};
-            for (var attr in obj)
-            {
-                if (obj.hasOwnProperty(attr))
-                    copy[attr] = clone(obj[attr]);
-            }
-            return copy;
-        }
+		// Handle Object
+		if (obj instanceof Object)
+		{
+			copy = Object.create(obj.__proto__);
+			for (var attr in obj)
+			{
+				if (obj.hasOwnProperty(attr))
+					copy[attr] = clone(obj[attr]);
+			}
 
-        throw new Error("Unable to clone object. Type unsupported.");
-    }
+			return copy;
+		}
+
+		throw new Error("Unable to clone object. Type unsupported.");
+	}
 	
 	/**
 		@class EntityFilter
